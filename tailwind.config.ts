c3678be7adc033a6d1c080ecb507c52814c7e095
Bulut Yerli <bulutyerli@gmail.com>
2024-08-29 20:22:01 +0300

    add bg-strech

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,9 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      backgroundSize: {
+        stretch: '100% 100%',
+      },
       colors: {
         main: '#121721',
         beige: {
